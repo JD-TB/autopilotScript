e6ecb33f2a14e58b11337e9d3c16910a78bf879a
JD-TB <56976798+JD-TB@users.noreply.github.com>
2020-07-21 14:13:04 -0400

    Update AutoPilot.cmd

--- AutoPilot.cmd
+++ AutoPilot.cmd
@@ -3,4 +3,4 @@ REM TODO: Download the latest version of "Get-WindowsAutoPilotInfo.ps1" and put 
 REM This grabs the info
 PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command %~d0\Get-WindowsAutoPilotInfo.ps1 -ComputerName $env:computername -OutputFile %~d0\computers.csv -append
 
-REM TODO: Email the .csv generated to an email specified in this file
+REM TODO: Email the .csv generated to an email specified in this file. see https://4bes.nl/2020/01/19/send-email-from-powershell-with-sendgrid/
