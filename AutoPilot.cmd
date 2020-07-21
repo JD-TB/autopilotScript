8f3812f55e8640d58241c5930f4cabcb46e3a19b
JD-TB <56976798+JD-TB@users.noreply.github.com>
2020-07-21 14:05:12 -0400

    Update AutoPilot.cmd

--- AutoPilot.cmd
+++ AutoPilot.cmd
@@ -1,1 +1,6 @@
+REM TODO: Download the latest version of "Get-WindowsAutoPilotInfo.ps1" and put it in the same directory as this file
+
+REM This grabs the info
 PowerShell -NoProfile -ExecutionPolicy Unrestricted -Command %~d0\Get-WindowsAutoPilotInfo.ps1 -ComputerName $env:computername -OutputFile %~d0\computers.csv -append
+
+REM TODO: Email the .csv generated to an email specified in this file
